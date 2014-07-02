75e2df48e0295b6babb4bb623cc1f677676c89cb
Robert Attaway <rattaway2@student.gsu.edu>
2014-07-02 12:41:04 -0400

    UI Framework
    Added the HTML file and CSS in order to create the mVc component of the
    application

--- CGameOfLIfe.js
+++ CGameOfLIfe.js
@@ -8,3 +8,25 @@
 //          1. Cells will have additional properties
 //          2. A random function will determine if a cell can evolve
 //          3. There will be various mutations that the cell can inherit
+//
+
+function ExtendedGameOfLife()
+{
+    this.displayReference = null;
+    this.displayState = "initial";
+    this.neighbors = [];
+    this.generation = -1;
+    this.evolutionaryTraits = [];
+    this.lifeExpectancy = -1;
+    // These are Conway's parameters. We are going to allow them to be dynamically
+    // modified if there is an evolutionary trait that allows for modification
+    this.minNeighbors = 2;
+    this.maxNeighbors = 3;
+}
+ExtendedGameOfLife.prototype={
+    constructor: ExtendedGameOfLife,
+    _displayFunction: null,
+    displayFunction: function(funcPointer){
+        _displayFunction = funcPointer;
+    }
+}
