3d2779b3a59a35a6becf8f1480fda38dacf832d1
Robert Attaway <rattaway2@student.gsu.edu>
2014-07-02 12:22:23 -0400

    Enhanced Game of Life
    This will define the actual game code independent of the UI. This code
    will be reusable because the only thing the data structure will need is
    a reference to the object to be updated along with a reference to an
    update function. The result is an MVC pattern that can allow the UI to
    be anything

---
+++ CGameOfLIfe.js
@@ -0,0 +1,10 @@
+// Game of life enhanced by Robert Attaway
+//
+// Purpose: Conway's Game of life was interesting but not robust. So I decided I wanted
+//          to create a more interesting version that would allow the game to use some
+//          principles of evolution to allow for more dynamic results
+//
+//          Instead of just implementing the basic rules the follwing extensions will apply
+//          1. Cells will have additional properties
+//          2. A random function will determine if a cell can evolve
+//          3. There will be various mutations that the cell can inherit
